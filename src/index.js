d0600907c2254c8a4d8fc14726dee0889390939c
vikasrajput18 <vikasvikas98809@gmail.com>
2023-08-16 21:21:29 +0530

    custom logger

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import app from "./app.js";
+import logger from "./configs/logger.config.js";
 
 // env variables
 const PORT = process.env.PORT || 8000;
@@ -6,5 +7,5 @@ const PORT = process.env.PORT || 8000;
 console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  logger.info(`Server is running on http://localhost:${PORT}`);
 });
